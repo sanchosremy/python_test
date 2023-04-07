16aae0fc2b4c523b1bfd225f097481e53ff7fe8e
Christian REMY <sachenkaremy@gmail.com>
2023-04-07 18:44:10 +0200

    thirnd commit

--- app.py
+++ app.py
@@ -2,3 +2,28 @@ print("Mosh Hamedani")
 print('o----')
 print("||||")
 print('*' * 10)
+price = 10
+price = 20
+print(price)
+nums = [12, 54, 34]
+print(max(nums))
+print(min(nums))
+Augustin = False
+Alexandre = True
+print(Augustin)
+print(Augustin + Alexandre)
+Denis = 9
+Anastasia = 5
+total = Denis + Anastasia
+print(total)
+first_name = 'John'
+last_name = 'Smith'
+full_name = first_name +' '+ last_name
+print(full_name)
+#name = input('What is your name?')
+#print(name,', Hi')
+#favorite_color = input('What is your favourite color?')
+#print(name + ' ' + 'likes ' + favorite_color)
+birth_year = input('birth_year: ')
+age = 2019 - int(birth_year)
+print(age)
