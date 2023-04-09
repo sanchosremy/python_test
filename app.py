3f1379d5336eb540752f490b85807fd6c307b9f7
Christian REMY <sachenkaremy@gmail.com>
2023-04-09 18:41:20 +0200

    four  commit

--- app.py
+++ app.py
@@ -24,6 +24,33 @@ print(full_name)
 #print(name,', Hi')
 #favorite_color = input('What is your favourite color?')
 #print(name + ' ' + 'likes ' + favorite_color)
-birth_year = input('birth_year: ')
-age = 2019 - int(birth_year)
-print(age)
+#birth_year = input('birth_year: ')
+#age = 2019 - int(birth_year)
+#print(age)
+#price = 10
+#price = 20
+#print(price)
+#weight_lbs = input('Votre poid (lbs): ')
+#weight_kg = int(weight_lbs) * 0.45
+#print(weight_kg)
+course = 'Python for "Beginners"'
+print(course)
+string ='''Hello
+My name is Alexandre and im studying in python
+Goodbye'''
+print(string)
+print(string[-1])
+name = 'Jennifer'
+print(name[1:0])
+first_name = 'John'
+last_name = 'Smith'
+message = first_name +' [' + last_name + '] is a coder'
+print(message)
+message = f'{first_name}  [{last_name}]  is a coder'
+print(message)
+print(len(message))
+print(message.upper())
+print(message.lower())
+print(message.find('c'))
+print(message.replace('[', '(').replace(']', ')'))
+print(message.replace('[Smith]', '(Smith)'))
