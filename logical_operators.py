2429658f7bd94744ff0733e8151b45a74ba943d4
Alexandre <sachenkaremy@gmail.com>
2023-04-18 18:51:14 +0200

    eight commit

--- logical_operators.py
+++ logical_operators.py
@@ -8,6 +8,4 @@ if HAS_GOOD_CREDIT and HAS_HIGHT_INCOME:
 if HAS_GOOD_CREDIT or HAS_HIGHT_INCOME:
     print("Eligible for loan")
 HAS_GOOD_CREDIT = True
-HAS_CRIMINAL_RECORD = True
-if HAS_GOOD_CREDIT and not HAS_CRIMINAL_RECORD:
-    print("Eligible for loan. Not criminal")
+HAS_CRIMINAL_RECORD = False
--- variables.py
+++ variables.py
@@ -1,3 +1,25 @@
 """
 Variables
 """
+print("Mosh Hamedani")
+print("o----")
+print("||||")
+print("*" * 10)
+PRICE = 10
+PRICE = 20
+print(PRICE)
+nums = [12, 54, 34]
+print(max(nums))
+print(min(nums))
+AUGUSTIN = False
+ALEXANDRE = True
+print(AUGUSTIN)
+print(AUGUSTIN + ALEXANDRE)
+DENIS = 9
+ANASTASIA = 5
+TOTAL = DENIS + ANASTASIA
+print(TOTAL)
+FIRST_NAME = "John"
+LAST_NAME = "Smith"
+FULL_NAME = FIRST_NAME + " " + LAST_NAME
+print(FULL_NAME)
