ceeb1cebadf01835ac78cbdafbd3bb2aef1b8019
Alexandre <sachenkaremy@gmail.com>
2023-04-18 18:21:02 +0200

    six commit

---
+++ if_statements.py
@@ -0,0 +1,13 @@
+"""If Statements
+"""
+IS_HOT = True
+IS_COLD = False
+if IS_HOT:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif IS_COLD:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
---
+++ math_function.py
@@ -0,0 +1,10 @@
+"""Math Functions
+"""
+import math
+
+X = 2.9
+print(round(X))
+X = 2.9
+print(abs(-2.9))
+print(math.ceil(2.9))
+print(math.floor(2.9))
