4143d99c0d3ad0c0c00b706b60758839e2392f55
Alexandre <sachenkaremy@gmail.com>
2023-05-09 18:45:43 +0200

    functions, keyword_argument

---
+++ functions.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-function-docstring
+"""Functions
+"""
+
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user("John", "Smith")
+print("Finish")
---
+++ keyword_arguments.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-function-docstring
+"""Keyword Arguments
+"""
+
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user(last_name="Smith", first_name="John")
+print("Finish")
