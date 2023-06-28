9a6c93b97f03800bfd2f51162920f966271222ed
sanchosremy <sachenkaremy@mail.com>
2023-06-28 18:02:13 +0300

    arithmetic_opperations_fun

---
+++ arithmetic_opperations_fun.py
@@ -0,0 +1,31 @@
+def calculate_division(first_number, second_number):
+    result = first_number / second_number
+    return result
+
+
+total = calculate_division(10, 3)
+print(total)
+
+
+def calculate_floor_division(first_number, second_number):
+    result = first_number // second_number
+    return result
+
+total = calculate_floor_division(10, 3)
+print(total)
+
+
+def calculate_modulus(first_number, second_number):
+    result = first_number % second_number
+    return result
+
+
+total = calculate_modulus(10, 3)
+print(total)
+
+
+def calculate_exponentiation(first_number, second_number):
+    result = first_number ** second_number
+    return result
+
+total = calculate_exponentiation(10, 3)
