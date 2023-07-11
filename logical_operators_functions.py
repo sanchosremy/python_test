f5a818ab89d3b91285a6518d88938f27a74e085d
sanchosremy <sachenkaremy@mail.com>
2023-07-11 15:59:49 +0300

    comparison_operators_functions, logical_operators_functions

--- logical_operators_functions.py
+++ logical_operators_functions.py
@@ -1,8 +1,5 @@
-def credit(has_good_credit, has_hight_income):
-    has_good_credit = True
-    has_hight_income = False
+def get_credit_rating(has_good_credit, has_hight_income):
     if has_good_credit and has_hight_income:
         print("Eligible for loan")
     if has_good_credit or has_hight_income:
         print("Eligible for loan")
-    has_good_credit = True
---
+++ while_loops_function.py
@@ -0,0 +1,5 @@
+def while_loops(character):
+    while character <= 5:
+        print("*" * character)
+        character = character + 1
+        print("Done")
